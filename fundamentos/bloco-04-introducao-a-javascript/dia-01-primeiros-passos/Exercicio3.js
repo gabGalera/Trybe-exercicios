31a85ed10796bac35de217b12168e179b74838ec
Gabriel Galera <gabgalera@hotmail.com>
2022-07-20 18:36:07 -0300

    Exercicio 2 ao 5

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-primeiros-passos/Exercicio3.js
@@ -0,0 +1,11 @@
+const a = 2**500;
+const b = 3000000000;
+const c = 10000;
+
+if (a>b && a>c){
+    console.log(a)
+} else if (b>a && b>c){
+    console.log(b)
+} else {
+    console.log(c)
+}
---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-primeiros-passos/Exercicio4.js
@@ -0,0 +1,10 @@
+const a = 0;
+
+if (a>0) {
+    console.log('positive')
+} else if (a<0) {
+    console.log('negative')
+} else {
+    console.log('zero')
+}
+
