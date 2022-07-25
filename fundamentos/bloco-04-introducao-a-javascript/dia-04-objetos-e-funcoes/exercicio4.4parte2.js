4c2065f1030df24a0858b09a7306c24413ac9446
Gabriel Galera <gabgalera@hotmail.com>
2022-07-25 12:29:20 -0300

    Exercicios do segundo objeto parte 2 requisito 2 e 3

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4parte2.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4parte2.js
@@ -1,23 +1,57 @@
 // Requisito 1
 
-let word = 'Gabriel';
-let palindromo = '';
+// let word = 'Gabriel';
 
-function verificaPalindrome(entry) { 
-    let j = entry.length;
-    for(i = 0; i < j; i++){
-        palindromo = palindromo + entry[j-i-1];
-    }
-    if(palindromo == entry){
-        return true
-    } else {
-        return false
-    }
-}
+// function verificaPalindrome(entry) { 
+//     let palindromo = '';
+//     let j = entry.length;
+//     for(i = 0; i < j; i++){
+//         palindromo = palindromo + entry[j-i-1];
+//     }
+//     if(palindromo == entry){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
 
-palindromo = verificaPalindrome(word);
+// palindromo = verificaPalindrome(word);
 
-console.log(palindromo);
+// console.log(palindromo);
 
 // Requisito 2
+
+let teste = [2, 3, 6, 7, 10, 1];
+
+function indiceMaior(entry) {
+    let maior = 0;
+    let index = 0;
+    for(i = 0; i < entry.length; i++){     
+        if(entry[i] > maior){
+            maior = entry[i];
+            index = i
+        }
+    }
+    return index
+}
+
+console.log(indiceMaior(teste));
+
+//Resquisito 3
+
+teste = [2, 4, 6, 7, 10, 0, -3];
+
+function indiceMenor(entry) {
+    let menor = 0;
+    let index = 0;
+    for(i = 0; i < entry.length; i++){     
+        if(entry[i] < menor){
+            maior = entry[i];
+            index = i
+        }
+    }
+    return index
+}
+
+console.log(indiceMenor(teste));
 
