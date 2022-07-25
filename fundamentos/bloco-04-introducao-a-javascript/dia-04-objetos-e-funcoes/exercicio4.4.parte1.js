bf8cc7e7a52d96f6f4b4c685c569d20c63d00c3f
Gabriel Galera <gabgalera@hotmail.com>
2022-07-25 10:48:12 -0300

    Exercicios do segundo objeto parte 1

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4.parte1.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4.parte1.js
@@ -33,3 +33,28 @@ for(let i in info[0]){
     console.log(info[0][i],"e",info[1][i]);
     }
 }
+
+// Objeto 2
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+    ],
+  };
+
+  console.log('O livro favorito de', leitor.nome, leitor.sobrenome, 
+  'se chama', leitor['livrosFavoritos'][0]['titulo']);
+  console.log(leitor.nome, 'tem', leitor.livrosFavoritos.length, 
+  'livros favoritos');
