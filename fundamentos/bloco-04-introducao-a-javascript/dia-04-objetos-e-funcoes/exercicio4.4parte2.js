6f3f8cf1408b9d23cba79b2b1322dbf16e18b83b
Gabriel Galera <gabgalera@hotmail.com>
2022-07-25 13:51:59 -0300

    Resquisito 5

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4parte2.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-objetos-e-funcoes/exercicio4.4parte2.js
@@ -21,7 +21,7 @@
 
 // Requisito 2
 
-let teste = [2, 3, 6, 7, 10, 1];
+// let teste = [2, 3, 6, 7, 10, 1];
 
 function indiceMaior(entry) {
     let maior = 0;
@@ -35,23 +35,59 @@ function indiceMaior(entry) {
     return index
 }
 
-console.log(indiceMaior(teste));
+// console.log(indiceMaior(teste));
 
-//Resquisito 3
+// //Resquisito 3
 
-teste = [2, 4, 6, 7, 10, 0, -3];
+// teste = [2, 4, 6, 7, 10, 0, -3];
 
-function indiceMenor(entry) {
-    let menor = 0;
-    let index = 0;
-    for(i = 0; i < entry.length; i++){     
-        if(entry[i] < menor){
-            maior = entry[i];
-            index = i
+// function indiceMenor(entry) {
+//     let menor = 0;
+//     let index = 0;
+//     for(i = 0; i < entry.length; i++){     
+//         if(entry[i] < menor){
+//             maior = entry[i];
+//             index = i
+//         }
+//     }
+//     return index
+// }
+
+// console.log(indiceMenor(teste));
+
+// Requisito 4
+
+// let testeS = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+// function maisCaracteres(entry){
+//     let maiorC = '';
+//     for(i = 0; i < entry.length; i++){
+//         if(entry[i].length > maiorC.length){
+//             maiorC = entry[i]
+//         }
+//     }
+//     return maiorC
+// }  
+
+// console.log(maisCaracteres(testeS));
+
+// Requisito 5
+
+let teste5 = [2, 3, 2, 5, 8, 2, 3];
+
+function mediana(entry){
+    let count = [];
+    for(i = 0; i < entry.length; i++){
+        count.push(0);
+        for(j = 0; j < entry.length; j++){
+            if(entry[i] == entry[j]){
+                count[i] = count[i] + 1 
+            }
         }
     }
-    return index
+
+    index = indiceMaior(count);
+    return entry[index]
 }
 
-console.log(indiceMenor(teste));
-
+console.log(mediana(teste5));
