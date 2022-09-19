8475d39ec8c8009ad1551a624f59a70f742ac71a
Gabriel Galera <gabgalera@hotmail.com>
2022-09-19 15:14:41 -0300

    Fixando os aprendizados concluídos

---
+++ Front-end/Bloco_10_Introducao_ao_React/primeiro-react-app/src/About.js
@@ -0,0 +1,14 @@
+const element =  
+<main>
+    <h1>Gabriel Galera</h1>
+        <p>Estudante de desenvolvimento web na Trybe</p>
+    <h2>Minhas habilidades</h2>
+        <ol>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Jest</li>
+        </ol>
+</main>;
+
+export default element;
