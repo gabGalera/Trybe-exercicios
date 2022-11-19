2cc3884aac39e819f9e0c9cde5d26fda96796a31
Gabriel Galera <gabgalera@hotmail.com>
2022-11-19 15:43:53 -0300

    Exercícios pronto

--- Front-end/Seção_9_Context_API_e_React_Hooks/Dia_01/first_forms/src/App.js
+++ Front-end/Seção_9_Context_API_e_React_Hooks/Dia_01/first_forms/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({ 
+    name: 'Meu nome', 
+    idade: null, 
+    cidade: ''
+})
+
+  const handleChange = ({ target }) => {
+    setState({
+      ...state,
+      [target.name]: target.value,
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <fieldset>
+        <label>
+          Nome: <input 
+          type="text" 
+          placeholder='Digite seu nome' 
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+          />
+        </label>
+        <label>
+          Idade: <input 
+          type="number" 
+          placeholder='Digite sua idade'
+          name="idade"
+          value={state.idade}
+          onChange={handleChange}
+          />
+        </label>
+        <label>
+          Cidade: <input 
+          type="text" 
+          placeholder='Digite sua cidade' 
+          name="cidade"
+          value={state.cidade}
+          onChange={handleChange}
+          />
+        </label>
+        <input 
+          type="radio" 
+          value="Fundamentos"
+          name="module" 
+          checked={state.module === "Fundamentos"}
+          onChange={handleChange}
+        /> Fundamentos
+        <input 
+          type="radio" 
+          value="Front-end" 
+          name="module" 
+          checked={state.module === "Front-end"}
+          onChange={handleChange}
+          /> Front-end
+        <input 
+          type="radio" 
+          value="Back-end" 
+          name="module" 
+          checked={state.module === "Back-end"}
+          onChange={handleChange}
+        /> Back-end 
+        <input 
+          type="radio" 
+          value="Ciência da computação" 
+          name="module" 
+          checked={state.module === "Ciência da computação"}
+          onChange={handleChange}
+        /> Ciência da computação
+    </fieldset>
+    </form>
   );
 }
 
