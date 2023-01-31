cdafb7810b15ffd9f1bbf5b86ca034591c0f1241
Gabriel Galera <gabgalera@hotmail.com>
2023-01-31 13:57:12 -0300

    Cria o método put

--- Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
+++ Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const teams = [
   {
     id: 1,
@@ -25,6 +27,21 @@ app.post('/teams', (req, res) => {
   teams.push(newTeam);
 
   res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team no found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
 });
 
 module.exports = app;
