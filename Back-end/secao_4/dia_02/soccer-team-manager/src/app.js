b25b5cfc0810444c97e548da168172105e83d435
Gabriel Galera <gabgalera@hotmail.com>
2023-01-31 13:36:30 -0300

    Cria tipes e os métodos get e post

--- Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
+++ Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
@@ -3,6 +3,28 @@ const express = require('express');
 
 const app = express();
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
 
 module.exports = app;
