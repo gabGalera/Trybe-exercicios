d1adf78db701e5c1cc5c5bee2a2839f8c1c1d31a
Gabriel Galera <gabgalera@hotmail.com>
2023-01-31 16:03:10 -0300

    Exercícios 3 e 4

--- Back-end/secao_4/dia_01/exerc/bmi.js
+++ Back-end/secao_4/dia_01/exerc/bmi.js
@@ -1,5 +1,7 @@
-const peso = 62;
-const altura = 1.64;
-const IMC = 62 / 1.64^2;
+const readline = require('readline-sync');
+
+const peso = readline.questionFloat('What is your weight? ')
+const altura = readline.questionFloat('What is your height? ');
+const IMC = peso / (altura^2);
 
 console.log(IMC);
