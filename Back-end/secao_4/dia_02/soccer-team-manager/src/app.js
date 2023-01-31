849fd6ea0eec82bc241dc176452c7390edfcbd62
Gabriel Galera <gabgalera@hotmail.com>
2023-01-31 14:03:18 -0300

    Cria o método delete

--- Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
+++ Back-end/secao_4/dia_02/soccer-team-manager/src/app.js
@@ -56,4 +56,12 @@ app.get('/teams/:id', (req, res) => {
   res.status(200).json({ ...queryTeam });
 });
 
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+
 module.exports = app;
