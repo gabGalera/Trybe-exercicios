47b0b1fe51e981b225f8fd0231b5103dc129c871
Gabriel Galera <gabgalera@hotmail.com>
2023-02-01 11:46:21 -0300

    Primeiro teste em chai

--- Back-end/secao_4/dia_03/cacau-trybe/tests/integration/chocolates.test.js
+++ Back-end/secao_4/dia_03/cacau-trybe/tests/integration/chocolates.test.js
@@ -8,7 +8,18 @@ chai.use(chaiHttp);
 
 describe('Usando o método GET em /chocolates', function () {
     it('Retorna a lista completa de chocolates!', async function () {
-        response = await minhaRequisicao();
-        expect(response.status).to.be.equal(200);
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+
+      response = await chai
+          .request(app)
+          .get('/chocolates');
+      expect(response.status).to.be.equal(200);
+
+      expect(response.body.chocolates).to.deep.equal(output);
     });
 });
