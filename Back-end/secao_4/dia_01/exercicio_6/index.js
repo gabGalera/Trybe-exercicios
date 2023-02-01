8454ed3bc7bb77b158e8204b96a303cc6d9f211d
Gabriel Galera <gabgalera@hotmail.com>
2023-02-01 16:23:03 -0300

    Exercício 6.A

--- Back-end/secao_4/dia_01/exercicio_6/index.js
+++ Back-end/secao_4/dia_01/exercicio_6/index.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 async function main() {
   try {
     const data = await fs.readFile('./simpsons.json', 'utf-8');
-    console.log(data);
+    JSON.parse(data).forEach((char) => console.log(`${char.id} - ${char.name}`))
   } catch (err) {
     console.error(`Erro ao ler o arquivo: ${err.message}`);
   }
