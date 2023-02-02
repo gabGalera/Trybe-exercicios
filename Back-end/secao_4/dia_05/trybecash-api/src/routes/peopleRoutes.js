47b6149e628a3ed58898893891bf4aa4b6958a35
Gabriel Galera <gabgalera@hotmail.com>
2023-02-02 15:26:32 -0300

    Faz a requisição

---
+++ Back-end/secao_4/dia_05/trybecash-api/src/routes/peopleRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+const peopleDB = require('../db/peopleDB');
+
+router.post('/', async (req, res) => {
+  const person = req.body;
+  try {
+    const [result] = await peopleDB.insert(person);
+    res.status(201).json({
+      message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
+  }
+});
+
+module.exports = router;
