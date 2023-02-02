352320a56ef14bdc5c5b8c26a97aa1f9b28e6984
Gabriel Galera <gabgalera@hotmail.com>
2023-02-02 15:38:03 -0300

    Requisições get

--- Back-end/secao_4/dia_05/trybecash-api/src/routes/peopleRoutes.js
+++ Back-end/secao_4/dia_05/trybecash-api/src/routes/peopleRoutes.js
@@ -3,6 +3,31 @@ const express = require('express');
 const router = express.Router();
 const peopleDB = require('../db/peopleDB');
 
+router.get('/', async (_req, res) => {
+  try {
+    const [result] = await peopleDB.findAll();
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.sqlMessage });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [[result]] = await peopleDB.findById(id);
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.sqlMessage });
+  }
+});
+
 router.post('/', async (req, res) => {
   const person = req.body;
   try {
--- Back-end/secao_4/dia_05/trybecash-api/tests/integration/people.test.js
+++ Back-end/secao_4/dia_05/trybecash-api/tests/integration/people.test.js
@@ -7,6 +7,24 @@ const connection = require('../../src/db/connection');
 
 const { expect, use } = chai;
 
+const peopleList = [
+  {
+    id: 1,
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    email: 'luke.skywalker@trybe.com',
+    phone: '851 678 4453',
+  },
+  {
+    id: 2,
+    firstName: 'Dart',
+    lastName: 'Vader',
+    email: 'dart.vader@trybe.com',
+    phone: '851 678 5665',
+  },
+];
+
+
 use(chaiHttp);
 
 describe('Testando os endpoints de people', function () {
@@ -30,5 +48,25 @@ describe('Testando os endpoints de people', function () {
       deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
   });
 
+  it('Testando a listagem de todas as pessoas', async function () {
+    sinon.stub(connection, 'execute').resolves([peopleList]);
+    const response = await chai
+      .request(app)
+      .get('/people');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(peopleList);
+  });
+
+  it('Testando a listagem da pessoa com id 1', async function () {
+    sinon.stub(connection, 'execute').resolves([[peopleList[0]]]);
+    const response = await chai
+      .request(app)
+      .get('/people/1');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(peopleList[0]);
+  });
+  
   afterEach(sinon.restore);
 });
