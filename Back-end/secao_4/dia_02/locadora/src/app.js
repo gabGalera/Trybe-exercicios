b0f39f061749e83c4b51a3f48e8c86c8837c111d
Gabriel Galera <gabgalera@hotmail.com>
2023-02-03 13:33:49 -0300

    Exercícios 6

--- Back-end/secao_4/dia_02/locadora/src/app.js
+++ Back-end/secao_4/dia_02/locadora/src/app.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/movies', async (req, res) => {
+  try {
+    const data = await readJSON();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
 app.get('/movies/:id', async (req, res) => {
   try {
     const data = await readJSON();
