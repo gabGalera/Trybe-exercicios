43d8d6c0ae6033a0b9ac2e18fb608270d982b4d9
Gabriel Galera <gabgalera@hotmail.com>
2023-02-04 11:50:55 -0300

    Exercícios 2 e 3

--- Back-end/secao_4/dia_04/exercicios_ecoturismo/src/app.js
+++ Back-end/secao_4/dia_04/exercicios_ecoturismo/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const { readJSON, writeJSON } = require('../utils/fsUtils');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/activities', async (req, res) => {
+  try {
+    const { body } = req;
+    await writeJSON(body);
+    res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 module.exports = app;
