27d34ec160e0551a257eaaef0fb322375fc90d8d
Gabriel Galera <gabgalera@hotmail.com>
2023-02-06 09:28:12 -0300

    Exercícios 8 e 9

--- Back-end/secao_4/dia_04/exercicios_ecoturismo/middlewares/validateActivities.js
+++ Back-end/secao_4/dia_04/exercicios_ecoturismo/middlewares/validateActivities.js
@@ -35,4 +35,56 @@ async function validateDescription(req, res, next) {
   }
 }
 
-module.exports = { validateName, validatePrice, validateDescription };
+function mustBeAValidDay(day) {
+  return Number(day) > 31 || Number(day) < 1; 
+}
+function mustBeAValidMonth(month) { 
+  return Number(month) > 12 || Number(month) < 1; 
+}
+
+function mustBeAValidYear(year) {
+  return Number(year) < 0;
+}
+async function validateCreatedAt(req, res, next) {
+  const { description: { createdAt } } = req.body;
+  const message = 'O campo createdAt deve ter o formato \'dd/mm/aaaa\'';
+  if (
+    !createdAt 
+    || mustBeAValidDay(createdAt.split('/')[0])
+    || mustBeAValidMonth(createdAt.split('/')[1])
+    || mustBeAValidYear(createdAt.split('/')[2])
+    ) {
+    res.status(400).json({ message });
+  } else {
+    next();
+  }
+}
+
+async function validateRating(req, res, next) {
+  const { description: { rating } } = req.body;
+  if (Number(rating) >= 1 && Number(rating) <= 5) {
+    next();
+  } else {
+    res.status(400).json({ message: 'O campo rating deve ser um número inteiro entre 1 e 5' });
+  }
+}
+
+async function validateDifficulty(req, res, next) {
+  const { description: { difficulty } } = req.body;
+  if (difficulty === 'Fácil' || difficulty === 'Médio' || difficulty === 'Difícil') {
+    next();
+  } else {
+    res.status(400).json({ 
+      message: 'O campo difficulty deve ser \'Fácil\', \'Médio\' ou \'Difícil\'', 
+    });
+  }
+}
+
+module.exports = { 
+  validateName, 
+  validatePrice,
+  validateDescription,
+  validateCreatedAt,
+  validateRating,
+  validateDifficulty,
+};
--- Back-end/secao_4/dia_04/exercicios_ecoturismo/src/app.js
+++ Back-end/secao_4/dia_04/exercicios_ecoturismo/src/app.js
@@ -3,42 +3,22 @@ const { readJSON, writeJSON } = require('../utils/fsUtils');
 const { 
   validateName, 
   validatePrice, 
-  validateDescription } = require('../middlewares/validateActivities');
+  validateDescription,
+  validateCreatedAt,
+  validateRating,
+  validateDifficulty } = require('../middlewares/validateActivities');
 
 const app = express();
 
 app.use(express.json());
 
-function mustBeAValidDay(day) {
-  return Number(day) > 31 || Number(day) < 1; 
-}
-function mustBeAValidMonth(month) { 
-  return Number(month) > 12 || Number(month) < 1; 
-}
-
-function mustBeAValidYear(year) {
-  return Number(year) < 0;
-}
-async function validateCreatedAt(req, res, next) {
-  const { description: { createdAt } } = req.body;
-  const message = 'O campo createdAt deve ter o formato \'dd/mm/aaaa\'';
-  if (
-    !createdAt 
-    || mustBeAValidDay(createdAt.split('/')[0])
-    || mustBeAValidMonth(createdAt.split('/')[1])
-    || mustBeAValidYear(createdAt.split('/')[2])
-    ) {
-    res.status(400).json({ message });
-  } else {
-    next();
-  }
-}
-
 app.post('/activities', 
   validateName, 
   validatePrice, 
   validateDescription, 
-  validateCreatedAt, 
+  validateCreatedAt,
+  validateRating, 
+  validateDifficulty,
   async (req, res) => {
     try {
       const { body } = req;
