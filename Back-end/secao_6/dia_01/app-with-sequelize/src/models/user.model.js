413e1c9d5b631ea335148e1baf911db02f9d9298
Gabriel Galera <gabgalera@hotmail.com>
2023-02-27 13:24:32 -0300

    Configura o sequelize

---
+++ Back-end/secao_6/dia_01/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,13 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+  (async () => {
+    await sequelize.sync({ force: true });
+    // As funções vão aqui
+  })();
+  return User;
+};
+
+module.exports = UserModel;
