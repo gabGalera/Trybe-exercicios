229f578d9aac1c41e3b90c892710267235893760
Gabriel Galera <gabgalera@hotmail.com>
2023-03-01 10:08:21 -0300

    Configurações iniciais

---
+++ Back-end/secao_6/dia_03/src/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql'
+}
+
+module.exports = {
+  development: {
+    ...config,
+    database: config.database + '_development',
+  },
+  test: {
+    ...config,
+    database: config.database + '_test',
+  },
+  production: {
+    ...config,
+    database: config.database + '_production',
+  },
+}
