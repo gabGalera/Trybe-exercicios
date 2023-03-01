e9a90f386b7e22feea51fae889ec8f2c70b6900c
Gabriel Galera <gabgalera@hotmail.com>
2023-03-01 10:24:20 -0300

    Configura a associação

--- Back-end/secao_6/dia_03/src/models/UserBook.js
+++ Back-end/secao_6/dia_03/src/models/UserBook.js
@@ -15,5 +15,20 @@ module.exports = (sequelize, _DataTypes) => {
     },
   );
 
+  UserBook.associate = (models) => {
+    models.Book.belongsToMany(models.User, {
+      as: 'users',
+      through: UserBook,
+      foreignKey: 'bookId',
+      otherKey: 'userId',
+    });
+    models.User.belongsToMany(models.Book, {
+      as: 'books',
+      through: UserBook,
+      foreignKey: 'userId',
+      otherKey: 'bookId',
+    });
+  };
+
   return UserBook;
 }
